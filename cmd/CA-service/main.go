89306e25eb13436f5d046d64e1f34ecfbd128b01
Peter Schrimpel <peter.schrimpel10@gmail.com>
2026-01-02 19:49:05 +0100

    feat: local HTTPS implemented and static API key parsing

--- cmd/CA-service/main.go
+++ cmd/CA-service/main.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"os"
 	"fmt"
+	"strings"
 
 	"github.com/Peto10/SSH-like-Certificate-Authority-Service/internal/api"
 	"github.com/Peto10/SSH-like-Certificate-Authority-Service/internal/server"
@@ -11,21 +12,44 @@ import (
 )
 
 const (
-	defaultServerHostName = ":8080"
+	defaultServerHostName = ":8443"
+	defaultURL			  = "https://localhost" + defaultServerHostName
+	certsDir              = "./certs/"
+	privateKeyFile        = certsDir + "localhost+2-key.pem"
+	publicCertFile        = certsDir + "localhost+2.pem"
+	envFilePath           = ".env"
 )
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	c := api.NewController(logger)
-	godotenv.Load("../.env")
 
-	c.Log.Info("service starting", "URL", fmt.Sprintf("http://localhost%s", defaultServerHostName))
+	godotenv.Load(envFilePath)
+	keys := parseStaticKeys("CA_ACCESS_TOKEN")
+	if len(keys) == 0 {
+		c.Log.Warn("No static keys parsed from environment variable")
+	}
+	fmt.Println("Parsed static keys:", keys)
+
+	c.Log.Info("service starting", "URL", defaultURL)
 
 	s := server.NewServer(c, defaultServerHostName)
 
-	err := s.ListenAndServe()
+	err := s.ListenAndServeTLS(publicCertFile, privateKeyFile)
 	if err != nil {
 		c.Log.Error("Error with listening and serving port", "error", err)
 		os.Exit(1)
 	}
+}
+
+func parseStaticKeys(envVarName string) map[string]struct{} {
+	keysStr := os.Getenv(envVarName)
+	keys := make(map[string]struct{})
+	if keysStr == "" {
+		return keys
+	}
+	for _, key := range strings.Split(keysStr, ",") {
+		keys[key] = struct{}{}
+	}
+	return keys
 }
